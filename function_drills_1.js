622ddbb1be57befac2dbc3619772078d7c2e1555
ebrocato <emilybrocato@outlook.com>
2019-04-29 13:19:20 -0600

    recent commit

--- function_drills_1.js
+++ function_drills_1.js
@@ -3,16 +3,40 @@
 
 
 
-function getYearOfBirth(age) {
-    return 2019 - age;
-}
+
+
 
 
 function createGreeting(name, age) {
-const yearOfBirth = getYearOfBirth(age);
- return 'hi my name is ' + name + ' and I am ' + age + ' years old.' + 'I was born in ' + yearOfBirth + '.';
+    const yearOfBirth = getYearOfBirth(age);
+    return 'hi my name is ' + name + ' and I am ' + age + ' years old.' + 'I was born in ' + yearOfBirth + '.';
 }
 
 
-const greeting1 = createGreeting("Emily", 27);
+
+function getYearOfBirth(age) {
+    if (age < 0) {
+        throw new Error("Age cannot be negative");
+        return;
+    }
+
+
+    try {
+        const greeting1 = createGreeting("Emily", -27);
+    } catch (e) {
+        console.log("Age cannot be negative");
+
+    }
+}
+// const greeting1 = createGreeting("Emily", -27);
 console.log(greeting1);
+
+
+// try { const greeting1 = createGreeting("Emily", 27); }
+
+// catch (e) {
+// }
+    
+
+
+// console.log(greeting1)
